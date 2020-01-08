177ad04dd19edf13e042edc28bb075452c881122
Jonathan Lignier <jonathan0780@hotmail.fr>
2020-01-08 13:35:41 +0100

    First commit

---
+++ Encrypt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ipro on 08/01/2020.
+//
+
+#include "Encrypt.h"
---
+++ Encrypt.h
@@ -0,0 +1,15 @@
+//
+// Created by ipro on 08/01/2020.
+//
+
+#ifndef UNTITLED1_ENCRYPT_H
+#define UNTITLED1_ENCRYPT_H
+
+using namespace std;
+
+class Encrypt {
+
+};
+
+
+#endif //UNTITLED1_ENCRYPT_H
